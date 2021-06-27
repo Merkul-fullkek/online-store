dd2bfc91c858d3118f997fc5baebbc3f7e98ecfb
Merkul-fullkek <84227189+Merkul-fullkek@users.noreply.github.com>
2021-06-27 20:45:23 +0300

    Back-end
    Back-end

---
+++ Server/middleware/ErrorHandlingMiddleware.js
@@ -0,0 +1,8 @@
+const APIerror = require('../Error/APIerror')
+
+module.exports = function (err, req, res, next) {
+    if(err instanceof APIerror){
+      return  res.status(err.status).json({message: err.message})
+    }
+    return res.status(500).json({message:'Непредвиденная ошибка'})
+}
